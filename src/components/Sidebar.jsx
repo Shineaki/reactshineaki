7b3cc5d3271009e8a5d8f8e0e6fca7784e680025
xUborka <xgurke@gmail.com>
2023-04-06 17:27:30 +0200

    fix: :ambulance: MOST IMPORTANT COMMIT

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -23,7 +23,7 @@ const Sidebar = () => {
         <>
           <div className='flex justify-between items-center'>
             <Link to="/"  onClick={handleCloseSideBar} className="items-center gap-3 ml-3 mt-4 flex text-xl font-extrabold tracking-tight text-slate-900">
-              <GiSelfLove /><span>Ádám és Domi</span>
+              <GiSelfLove /><span>Domi és Ádám</span>
             </Link>
           </div>
           <div className='mt-10'>
