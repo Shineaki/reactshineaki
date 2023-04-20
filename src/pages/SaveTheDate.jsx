3a0d20723c4a1f3f1330c178e40c8b795fa1d8e8
xUborka <xgurke@gmail.com>
2023-04-20 22:48:29 +0200

    Save the date image

--- src/pages/SaveTheDate.jsx
+++ src/pages/SaveTheDate.jsx
@@ -1,16 +1,16 @@
 import React from 'react';
-import domiadam_1 from '../data/1.png';
+import save_the_date_image from '../data/save_the_date.png';
 // import domiadam_2 from '../data/2.png';
 // import domiadam_3 from '../data/3.png';
 
 function SaveTheDate(){
-  return (
-    <div className='flex flex-col xl:flex-row justify-center items-center mt-10'>
-      <div className='flex basis-1/2 text-center m-5 p-10'>
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Modi est fugiat suscipit. Ipsum autem quas, porro esse velit ipsa numquam illum explicabo odit labore, doloremque et consectetur dicta tempore alias!
+  return (// xl:flex-row basis-1/2
+    <div className='flex flex-col  justify-center items-center mt-10'>
+      <div className='flex w-1/2 justify-center m-5'>
+        <img src={save_the_date_image} className='' alt=''/>
       </div>
-      <div className='flex basis-1/2 justify-center m-5'>
-        <img src={domiadam_1} className='w-500' alt='AdamDomi_1'/>
+      <div className='flex w-1/2 text-center m-5 p-10'>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Modi est fugiat suscipit. Ipsum autem quas, porro esse velit ipsa numquam illum explicabo odit labore, doloremque et consectetur dicta tempore alias!
       </div>
     </div>
   )
