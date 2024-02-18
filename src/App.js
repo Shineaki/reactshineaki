0efd5e702dc538ddb69f2846cfc292877f078c95
Shineaki <halmosadam92@gmail.com>
2024-02-18 14:41:31 +0100

    Changes #1

--- src/App.js
+++ src/App.js
@@ -6,62 +6,14 @@ import {
   Music, Galery, Schedule, Responses, Service
 } from './pages';
 import './App.css';
-import save_the_date_image from './data/save_the_date.png';
-import { FcGoogle } from 'react-icons/fc';
 import { useStateContext } from './contexts/ContextProvider';
-import { auth, googleAuthProvider } from "./components/FirebaseApp";
-import { signInWithPopup } from "firebase/auth";
-import jwt_decode from "jwt-decode";
-import { useEffect } from 'react';
 import Seating from './pages/Seating';
 
 
 
 function App() {
   const queryClient = new QueryClient();
-  const { token, setToken, activeMenu } = useStateContext();
-  
-  useEffect(() => {
-    const tokenString = sessionStorage.getItem('token');
-    if (tokenString != null){
-      const userToken = JSON.parse(tokenString);
-      var decoded = jwt_decode(userToken);
-      setToken(decoded);
-    }
-  }, [setToken])
-
-  function storeToken(user) {
-    user.getIdToken().then((result) => {
-      sessionStorage.setItem('token', JSON.stringify(result));
-      var decoded = jwt_decode(result);
-      setToken(decoded);
-    })
-  }
-
-  if (token == null) {
-    return (<div>
-      <div className='flex flex-col items-center p-10'>
-        <div className='flex w-full xl:w-3/4 justify-center'>
-          <img src={save_the_date_image} className='' alt='' />
-        </div>
-        <div className='flex w-full xl:w-3/4 justify-center'>
-          <button type="button" onClick={() => {
-            signInWithPopup(auth, googleAuthProvider).then((result) => {
-              console.log(result.user);
-              storeToken(result.user);
-            });
-          }} className='flex items-center justify-center w-48 
-    p-3 m-2  text-center bg-gray-50 transition-colors
-    duration-200 transform border rounded-lg
-    hover:bg-gray-200 drop-shadow-md'>
-            <span className='flex items-center p-1'><FcGoogle className='mr-3 text-2xl' /> Bejelentkezés</span>
-          </button>
-        </div>
-      </div>
-    </div>)
-  }
-
-
+  const { activeMenu } = useStateContext();
 
   return (
     <QueryClientProvider client={queryClient}>
--- src/pages/SaveTheDate.jsx
+++ src/pages/SaveTheDate.jsx
@@ -3,14 +3,25 @@ import save_the_date_image from '../data/save_the_date_2.png';
 // import domiadam_2 from '../data/2.png';
 // import domiadam_3 from '../data/3.png';
 
-function SaveTheDate(){
+function SaveTheDate() {
   return (
     <div className='flex flex-col items-center'>
       <div className='flex w-full justify-center mb-5'>
-        <img src={save_the_date_image} className='w-3/4 md:w-2/5' alt=''/>
+        <img src={save_the_date_image} className='w-3/5 md:w-3/5 lg:w-1/2 xl:w-1/3' />
       </div>
-      <div className='flex w-full md:w-2/3  text-center'>
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Modi est fugiat suscipit. Ipsum autem quas, porro esse velit ipsa numquam illum explicabo odit labore, doloremque et consectetur dicta tempore alias!
+      <div className='flex flex-col w-full md:w-2/3 text-center p-5 montserrat text-lg'>
+        <div className='pb-3 font-bold'>
+          Kedves Családunk és Barátaink!
+        </div>
+        <div className='pb-3'>
+          Örömmel tudjatjuk veletek, hogy (újra) kimondjuk a boldogító igent. Ezen apropóból szeretettel várunk benneteket 2024. április 20-án a bogádi Gállfy Birtokon tartandó esküvőnkre.
+        </div>
+        <div className='pb-3 font-bold'>
+          Részvételi szándékotokat és speciális étkezési igényeteket kérjük, hogy a "Visszejelzés" menüpont alatt jelezzétek!
+        </div>
+        <div className='mb-5'>
+          Alig várjuk, hogy együtt ünnepeljünk Veletek! 🥰
+        </div>
       </div>
     </div>
   )
