35439617b293de82a095542f618b54e479a4fc5b
Shineaki <halmosadam92@gmail.com>
2024-03-04 23:48:17 +0100

    Visszajelzés init

--- src/pages/Responses.jsx
+++ src/pages/Responses.jsx
@@ -1,14 +1,25 @@
 import React from 'react'
 import { useStateContext } from '../contexts/ContextProvider';
 
-function Responses(){
+function Responses() {
   const { user } = useStateContext();
-    if (user != null)
-    {
-      return <div>{user.displayName}</div>
-    }
+  if (user != null) {
+    return <div>{user.displayName}</div>
+  }
   return (
-    <div>Responses</div>
+    <div className='flex flex-col items-center m-10'>
+      <div className='text-2xl font-bold p-5'>
+        Visszajelzés
+      </div>
+      <div className='text-center'>
+        Ahogy a meghívás során beszéltük, <br />
+        családonként egy főt szeretnénk megkérni arra,<br />
+        hogy töltse ki az alábbi formot:<br />
+        <a href='https://ndominica.typeform.com/to/p8lLJcUY' className='text-blue-600 text-xl font-bold'>
+          Kattints ide!
+        </a>
+      </div>
+    </div>
   )
 }
 
